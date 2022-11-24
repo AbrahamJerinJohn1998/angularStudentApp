c124c1a5bd7dfb25973e3c60664e78206395e62f
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-24 14:45:59 +0530

    View all students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { ViewAllStudentsComponent } from './view-all-students/view-all-students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentEntryComponent
+    StudentEntryComponent,
+    ViewAllStudentsComponent
   ],
   imports: [
     BrowserModule,
