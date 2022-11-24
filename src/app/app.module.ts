a8a6a7733ca38d0b2157ef1e4415a4ea38d95c43
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-24 14:54:22 +0530

    Student delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { ViewAllStudentsComponent } from './view-all-students/view-all-students.component';
 import { StudentSearchComponent } from './student-search/student-search.component';
+import { StudentdeleteComponent } from './studentdelete/studentdelete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentEntryComponent,
     ViewAllStudentsComponent,
-    StudentSearchComponent
+    StudentSearchComponent,
+    StudentdeleteComponent
   ],
   imports: [
     BrowserModule,
